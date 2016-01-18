e53c9844b115f7191d7b9e1959065d3b536f2e30
Myk Willis <myk@mykwillis.com>
2016-01-17 19:19:50 -0500

    Break out karma-debug.conf.js to allow debugging of test cases in WebStorm.

--- karma-debug.conf.js
+++ karma-debug.conf.js
@@ -1,5 +1,8 @@
 /* License: MIT.
  * Copyright (C) 2016, Myk Willis.
+ *
+ * This file omits the coverage preprocessor, which allows test cases to be
+ * debugged from within WebStorm.
  */
 
 'use strict';
@@ -8,10 +11,9 @@ module.exports = function (config) {
 	config.set({
 		basePath: '',
 		frameworks: ['jasmine'],
-		logLevel: config.LOG_INFO,
+		logLevel: config.LOG_DEBUG,
 		browsers: ['PhantomJS'],
 		autoWatch: true,
-		reporters: ['dots', 'coverage'],
 		files: [
       'bower_components/jquery/dist/jquery.js',
 			'bower_components/angular/angular.js',
@@ -20,13 +22,6 @@ module.exports = function (config) {
       'bower_components/slick-carousel/slick/slick.js',
 			'angular-slick.js',
 			'tests.js'
-		],
-		preprocessors: {
-			'angular-slick.js': 'coverage'
-		},
-		coverageReporter: {
-			type: 'lcov',
-			dir: 'coverage/'
-		}
+		]
 	});
 };
